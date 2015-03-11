60ad77bdfec334d6294bd3749a8245bd9b88e789
Taylor Kaplan <taylor_kaplan@yahoo.com>
2015-03-11 01:33:46 -0700

    It sort of works

--- manifests/spark-master.pp
+++ manifests/spark-master.pp
@@ -1,8 +1,12 @@
-include base
+class { 'base':
+	scala_version => '10.5'
+}
+
 class { 'yarn':
 	hadoop_included => true,
 	master => true
 }
+
 class { 'spark':
 	with_yarn => true,
 	slave => false,
--- manifests/spark-slave.pp
+++ manifests/spark-slave.pp
@@ -1,8 +1,12 @@
-include base
+class { 'base':
+	scala_version => '10.5'
+}
+
 class { 'yarn':
 	hadoop_included => true,
 	master => false
 }
+
 class { 'spark':
 	with_yarn => true,
 	slave => true,
